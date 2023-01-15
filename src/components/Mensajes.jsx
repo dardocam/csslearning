ef5d24da10b89c56afe87dc0cca7008043342779
dardo camaño <dardocam@gmail.com>
2023-01-15 16:59:23 -0300

    add carrousel

--- src/components/Mensajes.jsx
+++ src/components/Mensajes.jsx
@@ -1,38 +1,64 @@
-import { useEffect, useState } from "react";
 import "../styles/Mensajes.css";
+import { BattleContext } from "../context/BattleContext";
+import { useContext, useEffect, useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 function Mensajes() {
+  const [battle] = useContext(BattleContext);
 
-  const [tips, setTips] = useState('')
+  const [mensaje, setMensaje] = useState([1, 2, 3]);
 
-  useEffect(()=>{
-    let i = 0;
+  useEffect(() => {
     const msj = [
       {
-        "id": 1,
-        "msj": "Primer tip"
+        id: 1,
+        msj: [
+          "primer tip battle 1",
+          "segundo tip battle 1",
+          "tercer tip battle 1",
+        ],
       },
       {
-        "id": 2,
-        "msj": "Segundo tip"
+        id: 2,
+        msj: [
+          "primer tip battle 2",
+          "segundo tip battle 2",
+          "tercer tip battle 2",
+        ],
       },
       {
-        "id": 3,
-        "msj": "Tercero tip"
+        id: 3,
+        msj: [
+          "primer tip battle 3",
+          "segundo tip battle 3",
+          "tercer tip battle 3",
+        ],
+      },
+    ];
+    msj.map((obj) => {
+      if (obj.id === parseInt(battle)) {
+        setMensaje(obj.msj);
+        return 0;
       }
-    ]
-    setInterval(() => {
-      if(i < msj.length){
-        setTips(msj[i++].msj)
-      }else{
-        i=0;
-      }
-    }, 2000);
-  },[])
+      return 0;
+    });
+  }, [battle]);
 
   return (
     <div className="mensajes">
-        {tips}
+      <Carousel
+        interval={5000}
+        infiniteLoop={true}
+        autoPlay={true}
+        showThumbs={false}
+        showStatus={false}
+        showArrows={false}
+      >
+        {
+          mensaje
+        }
+      </Carousel>
     </div>
   );
 }
